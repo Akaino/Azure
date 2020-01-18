e70a772ff9cedde312ff0c434d3a6a9f309ed2c9
Akaino <kaifroth@googlemail.com>
2020-01-18 16:08:31 +0100

    authfile.example.json

---
+++ Module 01/Demo 01 - Create a vm in netcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo_01___Create_a_vm_in_netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
