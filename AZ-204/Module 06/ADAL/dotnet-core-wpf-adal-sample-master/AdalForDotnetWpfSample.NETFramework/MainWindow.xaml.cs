53aa3e18f17436072917ed032b955aa8d460c699
Kai Roth <kai.roth@adn.de>
2020-04-20 14:36:55 +0200

    sampleMSAL

---
+++ AZ-204/Module 06/ADAL/dotnet-core-wpf-adal-sample-master/AdalForDotnetWpfSample.NETFramework/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Windows;
+
+namespace AdalForDotnetWpfSample.NETFramework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            var clientID = "";
+            var tenantID = "";
+            var authContext = new AuthenticationContext("https://login.microsoftonline.com/common");
+            var result = await authContext.AcquireTokenAsync("https://graph.microsoft.com", $"{clientID}", new Uri($"https://login.microsoftonline.com/{tenantID}/oauth2/v2.0/authorize/"), new PlatformParameters(PromptBehavior.Auto));
+            MessageBox.Show(result.AccessToken);
+        }
+    }
+}
