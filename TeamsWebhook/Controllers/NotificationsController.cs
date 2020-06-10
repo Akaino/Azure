d3ec274e11ef639374ee4c44790586073799b4e1
Kai Roth <kai.roth@adn.de>
2020-06-10 10:56:07 +0200

    refactoring

--- TeamsWebhook/Controllers/NotificationsController.cs
+++ TeamsWebhook/Controllers/NotificationsController.cs
@@ -11,16 +11,14 @@ using TeamsWebhook.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net;
-
-using System.Net.Http.Formatting;
 using Microsoft.Graph;
+using System.Net.Http.Formatting;
 
-using Microsoft.Identity.Client;
 using System.Net.Http.Headers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
-using Microsoft.Azure.Cosmos;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 
 namespace TeamsWebhook.Controllers
 {
@@ -34,24 +32,30 @@ namespace TeamsWebhook.Controllers
     private readonly ILogger<NotificationsController> _logger;
     private static Dictionary<string, Subscription> Subscriptions = new Dictionary<string, Subscription>();
     private static Timer subscriptionTimer = null;
-    private CosmosClient cosmosClient;
-    private Database database;
-    private Container container;
-    // public NotificationsController(MyConfig config)
-    // {
-    //   this.config = config;
-    // }
+    private static CosmosHelper cosmos;
+    private GraphServiceClient graphClient;
+    private TelemetryClient telemetryClient;
     public NotificationsController(IConfiguration config, ILogger<NotificationsController> logger)
     {
         _logger = logger;
         //logger.LogWarning("NotificationsController called!");
         this.Configuration = config;
+        if (cosmos == null) {
+            cosmos = new CosmosHelper(config);
+        }
+        if (this.telemetryClient == null) {
+            TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+            this.telemetryClient = new TelemetryClient(configuration);
+        }
     }
 
     [HttpGet]
     public ActionResult<string> Get()
     {
-      var graphServiceClient = GetGraphClient();
+        telemetryClient.TrackPageView("Notifications");
+        if (graphClient == null) {
+            graphClient = GraphHelper.Instance.GetGraphClient();
+        }
 
         var sub = new Microsoft.Graph.Subscription();
         sub.ChangeType = "created, updated";
@@ -63,10 +67,10 @@ namespace TeamsWebhook.Controllers
         sub.ClientState = "SecretClientState";
         //Console.WriteLine(sub.NotificationUrl);
 
-        var newSubscription = graphServiceClient
-        .Subscriptions
-        .Request()
-        .AddAsync(sub).Result;
+        var newSubscription = graphClient
+            .Subscriptions
+            .Request()
+            .AddAsync(sub).Result;
 
         Subscriptions[newSubscription.Id] = newSubscription;
 
@@ -74,9 +78,9 @@ namespace TeamsWebhook.Controllers
         {
             subscriptionTimer = new Timer(CheckSubscriptions, null, 5000, 15000);
         }
-
-        return $"Subscribed. Id: {newSubscription.Id}, Expiration: {newSubscription.ExpirationDateTime}";
-    }
+        var html = $"<html><p>Subscribed.</p><p>Id: {newSubscription.Id}</p><p>Expiration: {newSubscription.ExpirationDateTime}</p></html>";
+        return $"{html}";
+    } // Get()
 
     public ActionResult<string> Post([FromQuery]string validationToken = null)
     {
@@ -85,7 +89,7 @@ namespace TeamsWebhook.Controllers
         {
             //Console.WriteLine($"Received Token: '{validationToken}'");
             //_logger.LogWarning($"Received Token: '{validationToken}'");
-            
+            telemetryClient.TrackEvent("Validation",new Dictionary<string, string>{{"ValidationToken",$"{validationToken}"}});
             return Ok(validationToken);
         }
 
@@ -99,21 +103,15 @@ namespace TeamsWebhook.Controllers
             var notifications = JsonConvert.DeserializeObject<Notifications>(content.GetAwaiter().GetResult());
             //Console.WriteLine(content.GetAwaiter().GetResult());
             
-            var graphClient = GetGraphClient();
-            var accessToken = GetAccessToken().Result;
-
-            if (notifications.Items.Count() > 0) {
-                var options = new CosmosClientOptions() { ConnectionMode = ConnectionMode.Gateway };
-                this.cosmosClient = new CosmosClient(this.Configuration["cosmosUri"], this.Configuration["cosmosKey"], options);
-                this.database = cosmosClient.GetDatabase(Configuration["cosmosDatabase"]);
-                this.container = cosmosClient.GetContainer(database.Id, Configuration["cosmosContainer"]);
+            if (this.graphClient == null) {
+                this.graphClient = GraphHelper.Instance.GetGraphClient();
             }
-
+            var accessToken = GraphHelper.Instance.GetAccessToken().Result;
 
             foreach(var notification in notifications.Items)
             {
-                _logger.LogWarning($"Received notification: '{notification.Resource}', {notification.ResourceData?.Id}");
-                
+                //_logger.LogWarning($"Received notification: '{notification.Resource}', {notification.ResourceData?.Id}");
+                telemetryClient.TrackEvent("Received notification", new Dictionary<string, string>{{$"{notification.Resource}",$"{notification.ResourceData?.Id}"}});
                 // Calls abrufen
                 string callRecordStr = notification.ResourceData?.Id.ToString();
                 
@@ -123,78 +121,22 @@ namespace TeamsWebhook.Controllers
                 var con = client.GetAsync($"https://graph.microsoft.com/v1.0/communications/callrecords/{callRecordStr}").Result;
                 
                 var jsonString = con.Content.ReadAsStringAsync().Result;
-                var json = JObject.Parse(jsonString);
                 
-                // cosmos
-                AddItemToContainerAsync(jsonString).GetAwaiter().GetResult();
+                // Update CosmosDB with notifications
+                cosmos.AddItemToContainerAsync(jsonString).GetAwaiter().GetResult();
 
-                string organizer;
-                if (json != null) {
-                    //Console.WriteLine($"##### ERROR CallRecord ID: {json["id"]}");
-
-                    if (json["organizer"]["user"].Type == JTokenType.Null) {
-                        organizer = json["organizer"]["phone"]["id"].ToString();
-                    } else {
-                        if (json["organizer"]["user"]["displayName"].Type == JTokenType.Null) {
-                            organizer = json["organizer"]["user"]["id"].ToString();
-                        } else {
-                            organizer = organizer = json["organizer"]["user"]["displayName"].ToString();
-                        }
-                    }
-
-                    //var organizer = json["organizer"]["user"]["displayName"] == null ? json["organizer"]["user"]["id"] : json["organizer"]["user"]["displayName"];
-                    //_logger.LogWarning($"CallRecord ID: {json["id"]}, Start: {json["startDateTime"]}, End: {json["endDateTime"]}, Initiator: {organizer}");
-
-                }
-
-
-            }
+            } // foreach
         }
-
         // use deltaquery to query for all updates
         CheckForUpdates();
-
         return Ok();
-    }
-
-    private GraphServiceClient GetGraphClient()
-    {
-      var graphClient = new GraphServiceClient(new DelegateAuthenticationProvider((requestMessage) => {
-
-          // get an access token for Graph
-          var accessToken = GetAccessToken().Result;
-
-          requestMessage
-              .Headers
-              .Authorization = new AuthenticationHeaderValue("bearer", accessToken);
-
-          return Task.FromResult(0);
-      }));
-
-      return graphClient;
-    }
-
-    private async Task<string> GetAccessToken()
-    {
-//      IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(config.AppId)
-        IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(Configuration["AppId"])
-//        .WithClientSecret(config.AppSecret)
-        .WithClientSecret(Configuration["AppSecret"])
-        //.WithAuthority($"https://login.microsoftonline.com/{config.TenantId}")
-        .WithAuthority($"https://login.microsoftonline.com/{Configuration["TenantId"]}")
-        .WithRedirectUri("https://daemon")
-        .Build();
-
-      string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
-
-      var result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
-      return result.AccessToken;
-    }
+    } // Post([FromQuery]string validationToken = null)
 
     private void CheckSubscriptions(Object stateInfo)
     {
         AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
 
+        telemetryClient.TrackEvent("CheckSubscription", new Dictionary<string, string>{{$"stateInfo",$"{stateInfo.ToString()}"}});
         // _logger.LogWarning($"Checking subscriptions {DateTime.Now.ToString("h:mm:ss.fff")}");
         // _logger.LogWarning($"Current subscription count {Subscriptions.Count()}");
 
@@ -206,14 +148,11 @@ namespace TeamsWebhook.Controllers
                 RenewSubscription(subscription.Value);
             }
         }
-    }
+    } // CheckSubscriptions(Object stateInfo)
 
     private void RenewSubscription(Subscription subscription)
     {
         //_logger.LogWarning($"Current subscription: {subscription.Id}, Expiration: {subscription.ExpirationDateTime}");
-
-        var graphServiceClient = GetGraphClient();
-
         //subscription.ExpirationDateTime = DateTime.UtcNow.AddMinutes(122);
 
         var subscription2 = new Subscription
@@ -221,25 +160,28 @@ namespace TeamsWebhook.Controllers
             ExpirationDateTime = DateTime.UtcNow.AddMinutes(Convert.ToDouble(Configuration["ExpirationDateTimeMinutes"]))
         };
 
-        var foo = graphServiceClient
+        var foo = graphClient
             .Subscriptions[subscription.Id]
             .Request()
             .UpdateAsync(subscription2).Result;
 
-        _logger.LogWarning($"Renewed subscription: {subscription.Id}, New Expiration: {subscription2.ExpirationDateTime}");
-    }
+        telemetryClient.TrackEvent("Renewed Subscription", new Dictionary<string, string>{
+                {$"Subscription",$"{subscription.Id}"},
+                {"New Expiration","{subscription2.ExpirationDateTime}"}
+            });
+        //_logger.LogWarning($"Renewed subscription: {subscription.Id}, New Expiration: {subscription2.ExpirationDateTime}");
+    } // RenewSubscription(Subscription subscription)
 
     private static object DeltaLink = null;
-
     private static IUserDeltaCollectionPage lastPage = null;
 
     private void CheckForUpdates()
     {
-        var graphClient = GetGraphClient();
-
+        //var graphClient = GetGraphClient();
+        
         // get a page of users
         var users = GetUsers(graphClient, DeltaLink);
-
+        if (users == null) return;
         // OutputUsers(users);
 
         // go through all of the pages so that we can get the delta link on the last page.
@@ -255,7 +197,7 @@ namespace TeamsWebhook.Controllers
         {
             DeltaLink = deltaLink;
         }
-    }
+    } // CheckForUpdates()
 
     private void OutputUsers(IUserDeltaCollectionPage users)
     {
@@ -264,7 +206,7 @@ namespace TeamsWebhook.Controllers
                 var message = $"User: {user.Id}, {user.GivenName} {user.Surname}";
                 //_logger.LogWarning(message);
             }
-    }
+    } // OutputUsers(IUserDeltaCollectionPage users)
 
     private IUserDeltaCollectionPage GetUsers(GraphServiceClient graphClient, object deltaLink)
     {
@@ -293,38 +235,6 @@ namespace TeamsWebhook.Controllers
 
         lastPage = page;
         return page;
-    }
-
-    // Cosmos
-    private async Task AddItemToContainerAsync(string httpResult)
-        {
-            var CallRecordModel = JsonConvert.DeserializeObject<CallRecordModel>(httpResult);
-            var callRecordModelResponse = await this.container.ReadItemStreamAsync(CallRecordModel.Id, new PartitionKey(CallRecordModel.Id));
-
-            if (callRecordModelResponse.StatusCode == HttpStatusCode.NotFound)
-            {
-                // Create an item in the container representing the Andersen family. Note we provide the value of the partition key for this item, which is "Andersen"
-                try
-                {
-                    var item = await this.container.CreateItemAsync(CallRecordModel, new PartitionKey(CallRecordModel.Id));
-                    Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", item.Resource.Id, item.RequestCharge);
-                
-                }
-                catch (System.Exception)
-                {
-                    Console.WriteLine("Item already exists. Probably...");
-                    ItemResponse<CallRecordModel> item = await this.container.ReplaceItemAsync<CallRecordModel>(CallRecordModel, CallRecordModel.Id);
-                }
-                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
-                
-            }
-            else
-            {
-                ItemResponse<CallRecordModel> item = await this.container.ReplaceItemAsync<CallRecordModel>(CallRecordModel, CallRecordModel.Id);
-                //ItemResponse<CallRecordModel> item = await this.container.ReadItemAsync<CallRecordModel>(CallRecordModel.Id, new PartitionKey(CallRecordModel.Id));
-                Console.WriteLine("Item in database with id: {0} already exists\n", item.Resource.Id, ". Upserted item.");
-            }
-        }
-
+    } // GetUsers(GraphServiceClient graphClient, object deltaLink)
   }
 }
---
+++ TeamsWebhook/Helper/CosmosHelper.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace TeamsWebhook {
+
+    class CosmosHelper {
+        private readonly IConfiguration Configuration;
+        private CosmosClient cosmosClient;
+        private Database database;
+        private Container container;
+        public CosmosHelper(IConfiguration config) {
+            this.Configuration = config;
+            var options = new CosmosClientOptions() { ConnectionMode = ConnectionMode.Gateway };
+            this.cosmosClient = new CosmosClient(this.Configuration["cosmosUri"], this.Configuration["cosmosKey"], options);
+            this.database = cosmosClient.GetDatabase(Configuration["cosmosDatabase"]);
+            this.container = cosmosClient.GetContainer(database.Id, Configuration["cosmosContainer"]);
+        }
+
+        // Cosmos
+    public async Task AddItemToContainerAsync(string httpResult)
+        {
+            var CallRecordModel = JsonConvert.DeserializeObject<CallRecordModel>(httpResult);
+            var callRecordModelResponse = await this.container.ReadItemStreamAsync(CallRecordModel.Id, new PartitionKey(CallRecordModel.Id));
+
+            if (callRecordModelResponse.StatusCode == HttpStatusCode.NotFound)
+            {
+                // Create an item in the container representing the Andersen family. Note we provide the value of the partition key for this item, which is "Andersen"
+                try
+                {
+                    var item = await this.container.CreateItemAsync(CallRecordModel, new PartitionKey(CallRecordModel.Id));
+                    Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", item.Resource.Id, item.RequestCharge);
+                
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("Item already exists. Probably...");
+                    ItemResponse<CallRecordModel> item = await this.container.ReplaceItemAsync<CallRecordModel>(CallRecordModel, CallRecordModel.Id);
+                }
+                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
+                
+            }
+            else
+            {
+                ItemResponse<CallRecordModel> item = await this.container.ReplaceItemAsync<CallRecordModel>(CallRecordModel, CallRecordModel.Id);
+                //ItemResponse<CallRecordModel> item = await this.container.ReadItemAsync<CallRecordModel>(CallRecordModel.Id, new PartitionKey(CallRecordModel.Id));
+                Console.WriteLine("Item in database with id: {0} already exists\n", item.Resource.Id, ". Upserted item.");
+            }
+        } // AddItemToContainerAsync(string httpResult)
+    } // class
+} // namespace
