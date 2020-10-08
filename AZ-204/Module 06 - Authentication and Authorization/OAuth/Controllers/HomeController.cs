4a40d8790dc0f7075cccffb24b35e60842c43dcb
Kai Roth <kai.roth@adn.de>
2020-10-08 11:38:24 +0200

    OAuth

---
+++ AZ-204/Module 06 - Authentication and Authorization/OAuth/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OAuth.Models;
+
+namespace OAuth.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GeneralLoginRequired()
+        {
+            return View();
+        }
+        [Authorize("GroupMembership")]
+        public IActionResult Groupmembership()
+        {
+            return View();
+        }
+
+        public IActionResult RequestClaims() {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
