76a8d7ee6df51f284e294eaac862bc429f53466f
ADNTobi <tobias.pitzschke@adn.de>
2021-04-20 11:11:18 +0200

    packageUpdate

--- AZ-204/Module 03 - Blob Storage/BlobAccess/Program.cs
+++ AZ-204/Module 03 - Blob Storage/BlobAccess/Program.cs
@@ -1,40 +1,50 @@
 ﻿using System;
-using Microsoft.Azure.Storage;
-using Microsoft.Azure.Storage.Blob;
+using Azure.Storage;
+using Azure.Storage.Blobs;
 using Newtonsoft.Json.Linq;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace BlobAccess
 {
-    class Program
+  class Program
+  {
+    static JObject azureConfig = JObject.Parse(File.ReadAllText("azureConfig.json"));
+
+    // Batch Account credentials
+    static String storageConnectionString = azureConfig.GetValue("StorageConnectionString").ToString();
+
+    static async Task<int> Main(string[] args)
     {
-        static JObject azureConfig = JObject.Parse(File.ReadAllText("azureConfig.json"));
-
-        // Batch Account credentials
-        static String storageConnectionString = azureConfig.GetValue("StorageConnectionString").ToString();
+      Console.WriteLine("Accessing Blob Storage");
+      string connStr = storageConnectionString;
+      var date = DateTime.Now;
+      
+      try
+      {
+        BlobContainerClient containerClient = new BlobContainerClient(connStr, date.Millisecond + "myblobcontainer");
+        await containerClient.CreateIfNotExistsAsync();
         
-        static async Task<int> Main(string[] args)
+        Console.WriteLine("Created Container");
+
+        try
         {
-            Console.WriteLine("Accessing Blob Storage");
-            string connStr = storageConnectionString;
-
-            CloudStorageAccount account = CloudStorageAccount.Parse(connStr);
-
-            CloudBlobClient client = account.CreateCloudBlobClient();
-
-            var date = DateTime.Now;
-
-            CloudBlobContainer container = client.
-                GetContainerReference(date.Millisecond + "myblobcontainer");
-            
-            var exists = await container.CreateIfNotExistsAsync();
-
-            CloudBlockBlob blob = container.
-                GetBlockBlobReference(date.Millisecond + "myblob.png");
-
-            await blob.UploadFromFileAsync("fileToUpload.png");
-            return 0;
+          BlobClient blobClient = containerClient.GetBlobClient(date.Millisecond + "myblob.png");
+          await blobClient.UploadAsync("fileToUpload.png");
+          
+          Console.WriteLine("Upload Completed");
         }
+        catch (Exception e)
+        {
+          Console.WriteLine("Blob Exception: " + e.Message);
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("OH SNAP!: " + e.Message);
+      }
+
+      return 0;
     }
+  }
 }
