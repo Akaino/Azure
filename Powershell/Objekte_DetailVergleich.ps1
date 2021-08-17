6baed7d0b6cdb74d6ecdb8080967d7302483acef
ADNTobi <tobias.pitzschke@adn.de>
2021-08-17 09:14:44 +0200

    Objekt Detailvergleich

---
+++ Powershell/Objekte_DetailVergleich.ps1
@@ -0,0 +1,10 @@
+﻿$processes = Get-Process
+$p1 = $processes[15] 
+$p2 = $processes[16] 
+
+$properties = ($p1 | Get-Member -MemberType Property | Select-Object -ExpandProperty Name)
+
+foreach ($property in $properties) 
+{
+  Compare-Object $p1 $p2 -Property "$property" | Format-Table -AutoSize
+}
